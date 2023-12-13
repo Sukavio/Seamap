092275d822094f1a14c2ffe5877c23bd9a3c15cd
Martin Sernow (msg) <ckudo16@gmail.com>
2023-12-13 23:17:56 +0100

    Spring Boot hinzugefügt

---
+++ src/main/kotlin/de/taichj/Seamap/SeamapApplication.kt
@@ -0,0 +1,11 @@
+package de.taichj.Seamap
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SeamapApplication
+
+fun main(args: Array<String>) {
+	runApplication<SeamapApplication>(*args)
+}
---
+++ src/test/kotlin/de/taichj/Seamap/SeamapApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.taichj.Seamap
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SeamapApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
